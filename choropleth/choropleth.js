d56bb3924223b07c6a80071a61d9a122123ec19b
justincohler <justincohler@gmail.com>
2019-03-17 22:44:21 -0500

    Update styling and add new buttons

--- choropleth/choropleth.js
+++ choropleth/choropleth.js
@@ -4,173 +4,183 @@ Source/Credit: https://bl.ocks.org/adamjanes/6cf85a4fd79e122695ebde7d41fe327f
 The above source served as the start for this SVG element in the larger visualization.
 */
 async function make_choropleth([us, chr]) {
+  d3.select("#wide")
+    .append("svg")
+    .attr("id", "choropleth")
+    .attr(
+      "height",
+      params.choropleth.height +
+        params.choropleth.margin.top +
+        params.choropleth.margin.bottom
+    )
+    .attr(
+      "width",
+      params.choropleth.width +
+        params.choropleth.margin.left +
+        params.choropleth.margin.right
+    );
 
-    d3.select("#wide")
-        .append("svg")
-        .attr("id", "choropleth")
-        .attr("height", params.choropleth.height + params.choropleth.margin.top + params.choropleth.margin.bottom)
-        .attr("width", params.choropleth.width + params.choropleth.margin.left + params.choropleth.margin.right);
+  var projection = d3
+    .geoMercator()
+    .scale(8500)
+    .center([-79.5, 39.5]);
 
-    var projection = d3.geoMercator().scale(8500).center([-79.5, 39.5]);
+  // Create a path generator.
+  var path = d3.geoPath().projection(projection);
 
-    // Create a path generator.
-    var path = d3.geoPath()
-        .projection(projection);
+  const x = d3
+    .scaleLinear()
+    .domain([1, 10])
+    .rangeRound([600, 860]);
 
-    const x = d3.scaleLinear()
-        .domain([1, 10])
-        .rangeRound([600, 860]);
+  let choropleth = d3.select("#choropleth");
 
-    let choropleth = d3.select("#choropleth");
+  // County shapes
+  choropleth
+    .append("g")
+    .attr("class", "counties")
+    .selectAll("path")
+    .data(
+      topojson.feature(us, us.objects.cb_2015_west_virginia_county_20m).features
+    )
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("id", d => {
+      return "poly-" + d.properties.GEOID;
+    });
 
-    // County shapes
-    choropleth.append("g")
-        .attr("class", "counties")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.cb_2015_west_virginia_county_20m).features)
-        .enter().append("path")
-        .attr("d", path)
-        .attr("id", (d) => {
-            return "poly-" + d.properties.GEOID
-        })
-
-    return chr
+  return chr;
 }
 
 async function update_choropleth(chr) {
+  let choropleth = d3.select("#choropleth");
 
-    let choropleth = d3.select("#choropleth");
+  // Tooltips
+  tip = d3
+    .tip()
+    .attr("class", "d3-tip")
+    .offset([-10, 0])
+    .direction("n")
+    .html(function(d) {
+      county = chr[d.properties.GEOID].county;
+      state = chr[d.properties.GEOID].state;
 
-    // Tooltips
-    tip = d3.tip()
-        .attr('class', 'd3-tip')
-        .offset([-10, 0])
-        .direction('n')
-        .html(function (d) {
-            county = chr[d.properties.GEOID].county;
-            state = chr[d.properties.GEOID].state;
+      text = county + ", " + state + "<br/>OD Mortality Rate: ";
+      text +=
+        chr[d.properties.GEOID].od_mortality_rate == ""
+          ? "N/A"
+          : chr[d.properties.GEOID].od_mortality_rate;
+      return text;
+    });
+  choropleth.call(tip);
 
-            text = county + ", " + state + "<br/>OD Mortality Rate: ";
-            text += chr[d.properties.GEOID].od_mortality_rate == "" ? "N/A" : chr[d.properties.GEOID].od_mortality_rate;
-            return text;
-        });
-    choropleth.call(tip);
+  // County shapes
+  choropleth
+    .selectAll("path")
+    .on("mouseover", function(d, i) {
+      tip.show(d, this);
 
-    // County shapes
-    choropleth.selectAll("path")
-        .on("mouseover", function (d, i) {
-            tip.show(d, this);
+      d3.select(this).style("cursor", "pointer");
 
-            d3.select(this)
-                .style("cursor", "pointer");
+      od_mortality_rate = chr[d.properties.GEOID].od_mortality_rate;
 
-            od_mortality_rate = chr[d.properties.GEOID].od_mortality_rate;
+      pctile = ntile(MAX_STAT, od_mortality_rate, NTILES);
 
-            pctile = ntile(MAX_STAT, od_mortality_rate, NTILES);
+      d3.select("#bar-" + pctile).attr("fill", YELLOW);
 
-            d3.select("#bar-" + pctile)
-                .attr("fill", YELLOW);
+      lineID = ("#line-" + chr[d.properties.GEOID].county).toLowerCase();
 
-            lineID = ("#line-" + chr[d.properties.GEOID].county).toLowerCase();
+      d3.select(lineID).classed("hoverLine", true);
+    })
+    .on("mouseout", d => {
+      tip.hide(d, this);
 
-            d3.select(lineID)
-                .classed("hoverLine", true);
+      od_mortality_rate = chr[d.properties.GEOID].od_mortality_rate;
+      pctile = ntile(MAX_STAT, od_mortality_rate, NTILES);
 
-        })
-        .on("mouseout", (d) => {
-            tip.hide(d, this);
+      lineID = ("#line-" + chr[d.properties.GEOID].county).toLowerCase();
 
-            od_mortality_rate = chr[d.properties.GEOID].od_mortality_rate;
-            pctile = ntile(MAX_STAT, od_mortality_rate, NTILES);
+      d3.select(lineID).classed("hoverLine", false);
 
-            lineID = ("#line-" + chr[d.properties.GEOID].county).toLowerCase();
-
-            d3.select(lineID)
-                .classed("hoverLine", false);
-
-            d3.select("#bar-" + pctile)
-                .transition()
-                .duration(200)
-                .attr("fill", function (d) { return bucketColorScale(pctile); })
-        })
-        .on("click", function (d) {
-            od_mortality_rate = chr[d.properties.GEOID].od_mortality_rate;
-            pctile = ntile(MAX_STAT, od_mortality_rate, NTILES);
-
-            /* CLICK OFF */
-            if (selected_counties.has(d.properties.GEOID)) {
-                selected_counties.delete(d.properties.GEOID);
-                console.log("Selected Counties: ", selected_counties);
-                // This is the last county unclicked
-                if (selected_counties.size == 0) {
-                    console.log("Making Everything opaque again");
-                    d3.selectAll(".counties")
-                        .classed("activeCounty", false)
-                        .classed("inactiveCounty", false);
-
-                    d3.select(this)
-                        .classed("activeCounty", false)
-                        .classed("inactiveCounty", false);
-
-                    d3.selectAll(".bar")
-                        .attr("fill-opacity", 0.9);
-                } else {
-                    d3.select(this)
-                        .classed("inactiveCounty", true)
-                        .classed("activeCounty", false);
-
-                    d3.select("#bar-" + pctile)
-                        .attr("fill-opacity", 0.3);
-
-                }
-            }
-            /* CLICK ON */
-            else {
-                // This is the first county clicked
-                if (selected_counties.size == 0) {
-                    d3.selectAll(`.counties`)
-                        .classed("inactiveCounty", true);
-
-                    d3.selectAll(".bar")
-                        .attr("fill-opacity", 0.3);
-                }
-                // Click "ON"
-                selected_counties.add(d.properties.GEOID);
-                console.log("Selected Counties: ", selected_counties);
-
-                d3.select("#bar-" + pctile)
-                    .attr("fill-opacity", 1.0);
-
-                d3.select(this)
-                    .classed("activeCounty", true);
-            }
-        })
+      d3.select("#bar-" + pctile)
         .transition()
-        .duration(2000)
-        .attr("fill", (d) => {
-            fips = d.properties.GEOID;
-            value = chr[fips] && chr[fips].od_mortality_rate != "" ? chr[fips].od_mortality_rate : 0;
-            return colorScale(value);
+        .duration(200)
+        .attr("fill", function(d) {
+          return bucketColorScale(pctile);
         });
+    })
+    .on("click", function(d) {
+      od_mortality_rate = chr[d.properties.GEOID].od_mortality_rate;
+      pctile = ntile(MAX_STAT, od_mortality_rate, NTILES);
 
-    // SOURCE
-    choropleth
-        .append("text")
-        .attr("class", "source")
-        .attr("x", params.choropleth.width / 2 - 50)
-        .attr("y", params.choropleth.height - 50)
-        .html("Source: CountyHealthRankings.org");
+      /* CLICK OFF */
+      if (selected_counties.has(d.properties.GEOID)) {
+        selected_counties.delete(d.properties.GEOID);
+        console.log("Selected Counties: ", selected_counties);
+        // This is the last county unclicked
+        if (selected_counties.size == 0) {
+          console.log("Making Everything opaque again");
+          d3.selectAll(".counties")
+            .classed("activeCounty", false)
+            .classed("inactiveCounty", false);
 
+          d3.select(this)
+            .classed("activeCounty", false)
+            .classed("inactiveCounty", false);
 
-    // Summary
-    // choropleth
-    //     .append("text")
-    //     .attr("class", "summary")
-    //     .attr("x", params.choropleth.width / 2 + 10)
-    //     .attr("y", params.choropleth.height / 2 + 110)
-    //     .html(`Drug Overdose Mortality Rates<br/>
-    //                 per 100k skyrocketed betweeen<br/>
-    //                 2014 and 2018.
-    //                 `);
+          d3.selectAll(".bar").attr("fill-opacity", 0.9);
+        } else {
+          d3.select(this)
+            .classed("inactiveCounty", true)
+            .classed("activeCounty", false);
 
+          d3.select("#bar-" + pctile).attr("fill-opacity", 0.3);
+        }
+      } else {
+      /* CLICK ON */
+        // This is the first county clicked
+        if (selected_counties.size == 0) {
+          d3.selectAll(`.counties`).classed("inactiveCounty", true);
+
+          d3.selectAll(".bar").attr("fill-opacity", 0.3);
+        }
+        // Click "ON"
+        selected_counties.add(d.properties.GEOID);
+        console.log("Selected Counties: ", selected_counties);
+
+        d3.select("#bar-" + pctile).attr("fill-opacity", 1.0);
+
+        d3.select(this).classed("activeCounty", true);
+      }
+    })
+    .transition()
+    .duration(2000)
+    .attr("fill", d => {
+      fips = d.properties.GEOID;
+      value =
+        chr[fips] && chr[fips].od_mortality_rate != ""
+          ? chr[fips].od_mortality_rate
+          : 0;
+      return colorScale(value);
+    });
+
+  // SOURCE
+  choropleth
+    .append("text")
+    .attr("class", "source")
+    .attr("x", params.choropleth.width / 2 - 50)
+    .attr("y", params.choropleth.height - 50)
+    .html("Source: CountyHealthRankings.org");
+
+  // Summary
+  // choropleth
+  //     .append("text")
+  //     .attr("class", "summary")
+  //     .attr("x", params.choropleth.width / 2 + 10)
+  //     .attr("y", params.choropleth.height / 2 + 110)
+  //     .html(`Drug Overdose Mortality Rates<br/>
+  //                 per 100k skyrocketed betweeen<br/>
+  //                 2014 and 2018.
+  //                 `);
 }
--- choropleth/toolbar.js
+++ choropleth/toolbar.js
@@ -8,13 +8,22 @@
 
 */
 
-regressors = []
+regressors = [];
 
 function drag(event) {
-    // console.log(event.target.id);
-    let text = regressors.length == 0 ? event.target.id : ", " + event.target.id;
-    regressors.push(event.target.id);
-    event.dataTransfer.setData("text", text);
-    event.dataTransfer.effectAllowed = "copyMove";
-    event.dataTransfer.dropEffect = "copy";
+  // console.log(event.target.id);
+  let text = regressors.length == 0 ? event.target.id : ", " + event.target.id;
+  regressors.push(event.target.id);
+  event.dataTransfer.setData("text", text);
+  event.dataTransfer.effectAllowed = "copyMove";
+  event.dataTransfer.dropEffect = "copy";
 }
+
+// const data = [[0, 1], [32, 67], [12, 79]];
+// const result = regression.polynomial(data, { order: 3 });
+
+const result = regression.linear([[0, 1], [32, 67], [12, 79]]);
+const gradient = result.equation[0];
+const yIntercept = result.equation[1];
+
+console.log("Result", result, "Gradient", gradient, "Y-Intercept", yIntercept);
